443f418bdaeaaf5ef13458e9761bf85a0ac535b6
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-05-16 04:20:52 +0900

    Update RedBlackTree

--- src/red_black_tree.rs
+++ src/red_black_tree.rs
@@ -1,149 +1,123 @@
 use std::cmp::{Ord, Ordering};
+use std::fmt;
 
-enum Node<T>
-where
-    T: Ord,
-{
+enum Node<T: Ord> {
     Nil,
     Red(T, Box<Node<T>>, Box<Node<T>>),
     Black(T, Box<Node<T>>, Box<Node<T>>),
 }
 
-struct RedBlackTree<T>
-where
-    T: Ord,
-{
+pub struct RedBlackTree<T: Ord> {
     root: Box<Node<T>>,
 }
 
 use Node::{Black, Nil, Red};
 
-impl<T> RedBlackTree<T>
-where
-    T: Ord,
-{
-    fn new() -> Self {
+impl<T: Ord> RedBlackTree<T> {
+    pub fn new() -> Self {
         Self {
             root: Box::new(Node::Nil),
         }
     }
 
     fn contains_inner(node: &Node<T>, value: T) -> bool {
-        let (node_value, left, right) = match node {
+        match node {
             Nil => return false,
-            Red(node_value, left, right) => (node_value, left, right),
-            Black(node_value, left, right) => (node_value, left, right),
-        };
-        match value.cmp(&node_value) {
-            Ordering::Less => Self::contains_inner(left, value),
-            Ordering::Equal => true,
-            Ordering::Greater => Self::contains_inner(right, value),
+            Red(node_value, left, right) | Black(node_value, left, right) => {
+                match value.cmp(&node_value) {
+                    Ordering::Less => Self::contains_inner(left, value),
+                    Ordering::Equal => true,
+                    Ordering::Greater => Self::contains_inner(right, value),
+                }
+            }
         }
     }
 
-    fn contains(&self, value: T) -> bool {
+    pub fn contains(&self, value: T) -> bool {
         Self::contains_inner(&self.root, value)
     }
 
     fn insert_inner(node: Box<Node<T>>, value: T) -> (bool, Box<Node<T>>) {
+        let nil = || Box::new(Nil);
+        let red = |value, left, right| Box::new(Red(value, left, right));
+        let black = |value, left, right| Box::new(Black(value, left, right));
+
         match *node {
-            Nil => (true, Box::new(Red(value, Box::new(Nil), Box::new(Nil)))),
+            Nil => (true, red(value, nil(), nil())),
             Red(node_value, left, right) => match value.cmp(&node_value) {
                 Ordering::Less => {
                     let (changed, left) = Self::insert_inner(left, value);
-                    (changed, Box::new(Red(node_value, left, right)))
+                    (changed, red(node_value, left, right))
                 }
-                Ordering::Equal => (false, Box::new(Red(node_value, left, right))),
+                Ordering::Equal => (false, red(node_value, left, right)),
                 Ordering::Greater => {
                     let (changed, right) = Self::insert_inner(right, value);
-                    (changed, Box::new(Red(node_value, left, right)))
+                    (changed, red(node_value, left, right))
                 }
             },
             Black(node_value, left, right) => match value.cmp(&node_value) {
                 Ordering::Less => {
                     let (changed, left) = Self::insert_inner(left, value);
                     if !changed {
-                        return (false, Box::new(Black(node_value, left, right)));
+                        return (false, black(node_value, left, right));
                     }
                     let u = node_value;
                     let t4 = right;
                     match *left {
+                        Nil => unreachable!(),
                         Red(v, left_left, left_right) => match (*left_left, *left_right) {
                             (Red(w, t1, t2), left_right) => {
                                 let t3 = Box::new(left_right);
-                                (
-                                    true,
-                                    Box::new(Red(
-                                        v,
-                                        Box::new(Black(w, t1, t2)),
-                                        Box::new(Black(u, t3, t4)),
-                                    )),
-                                )
+                                (true, red(v, black(w, t1, t2), black(u, t3, t4)))
                             }
                             (left_left, Red(w, t2, t3)) => {
                                 let t1 = Box::new(left_left);
-                                (
-                                    true,
-                                    Box::new(Red(
-                                        w,
-                                        Box::new(Black(v, t1, t2)),
-                                        Box::new(Black(u, t3, t4)),
-                                    )),
-                                )
+                                (true, red(w, black(v, t1, t2), black(u, t3, t4)))
                             }
                             (left_left, left_right) => (
-                                false,
-                                Box::new(Red(v, Box::new(left_left), Box::new(left_right))),
+                                true,
+                                black(u, red(v, Box::new(left_left), Box::new(left_right)), t4),
                             ),
                         },
-                        _ => unreachable!(),
+                        Black(v, left_left, left_right) => {
+                            (true, black(u, black(v, left_left, left_right), t4))
+                        }
                     }
                 }
-                Ordering::Equal => (false, Box::new(Black(node_value, left, right))),
+                Ordering::Equal => (false, black(node_value, left, right)),
                 Ordering::Greater => {
                     let (changed, right) = Self::insert_inner(right, value);
                     if !changed {
-                        return (false, Box::new(Black(node_value, left, right)));
+                        return (false, black(node_value, left, right));
                     }
                     let u = node_value;
                     let t1 = left;
                     match *right {
+                        Nil => unreachable!(),
                         Red(v, right_left, right_right) => match (*right_left, *right_right) {
                             (Red(w, t2, t3), right_right) => {
                                 let t4 = Box::new(right_right);
-                                (
-                                    true,
-                                    Box::new(Red(
-                                        w,
-                                        Box::new(Black(u, t1, t2)),
-                                        Box::new(Black(v, t3, t4)),
-                                    )),
-                                )
+                                (true, red(w, black(u, t1, t2), black(v, t3, t4)))
                             }
                             (right_left, Red(w, t3, t4)) => {
                                 let t2 = Box::new(right_left);
-                                (
-                                    true,
-                                    Box::new(Red(
-                                        v,
-                                        Box::new(Black(u, t1, t2)),
-                                        Box::new(Black(w, t3, t4)),
-                                    )),
-                                )
+                                (true, red(v, black(u, t1, t2), black(w, t3, t4)))
                             }
                             (right_left, right_right) => (
-                                false,
-                                Box::new(Red(v, Box::new(right_left), Box::new(right_right))),
+                                true,
+                                black(u, t1, red(v, Box::new(right_left), Box::new(right_right))),
                             ),
                         },
-                        _ => unreachable!(),
+                        Black(v, right_left, right_right) => {
+                            (true, black(u, t1, black(v, right_left, right_right)))
+                        }
                     }
                 }
             },
         }
     }
 
-    fn insert(&mut self, value: T) -> bool {
+    pub fn insert(&mut self, value: T) -> bool {
         let root = std::mem::replace(&mut self.root, Box::new(Nil));
         let (changed, root) = Self::insert_inner(root, value);
         if !changed {
@@ -151,21 +125,56 @@ where
             return false;
         }
         match *root {
-            Red(node_value, left, right) => {
+            Nil => unreachable!(),
+            Red(node_value, left, right) | Black(node_value, left, right) => {
                 self.root = Box::new(Black(node_value, left, right));
                 true
             }
-            _ => unreachable!(),
         }
     }
 }
 
-#[test]
-fn test_red_black_tree() {
-    let mut tree = RedBlackTree::new();
-    tree.insert(1);
-    assert_eq!(tree.contains(1), true);
-    assert_eq!(tree.contains(2), false);
-    tree.insert(2);
-    assert_eq!(tree.contains(2), true);
+impl<T: Ord + fmt::Debug> fmt::Debug for RedBlackTree<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        writeln!(f, "RedBlackTree {{")?;
+        let mut stack = vec![];
+        stack.push((0, self.root.as_ref()));
+        while let Some((depth, node)) = stack.pop() {
+            match node {
+                Nil => (),
+                Red(value, left, right) => {
+                    write!(f, "{}", "    ".repeat(1 + depth))?;
+                    writeln!(f, "Red({:?})", value)?;
+                    stack.push((depth + 1, right));
+                    stack.push((depth + 1, left));
+                }
+                Black(value, left, right) => {
+                    write!(f, "{}", "    ".repeat(1 + depth))?;
+                    writeln!(f, "Black({:?})", value)?;
+                    stack.push((depth + 1, right));
+                    stack.push((depth + 1, left));
+                }
+            }
+        }
+        writeln!(f, "}}")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::RedBlackTree;
+
+    #[test]
+    fn test_red_black_tree() {
+        let mut tree = RedBlackTree::new();
+        for i in 0..100 {
+            if i % 2 == 0 {
+                assert_eq!(tree.insert(i), true);
+            }
+        }
+        for i in 0..100 {
+            assert_eq!(tree.contains(i), i % 2 == 0);
+        }
+        dbg!(&tree);
+    }
 }
