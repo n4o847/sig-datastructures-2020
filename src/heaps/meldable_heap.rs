e663b57fe7d63d13f58b287746fcdeb2175ef8f5
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-05-23 01:13:38 +0900

    Improve memory efficiency

--- src/heaps/meldable_heap.rs
+++ src/heaps/meldable_heap.rs
@@ -2,44 +2,40 @@ use std::cmp::Ord;
 use std::fmt;
 use std::mem;
 
-enum Node<T: Ord> {
-    Nil,
-    Cons(T, Box<Node<T>>, Box<Node<T>>),
+struct Node<T: Ord> {
+    value: T,
+    left: Option<Box<Node<T>>>,
+    right: Option<Box<Node<T>>>,
 }
 
 pub struct MeldableHeap<T: Ord> {
-    root: Box<Node<T>>,
+    root: Option<Box<Node<T>>>,
     len: usize,
 }
 
-use Node::{Cons, Nil};
-
 impl<T: Ord> MeldableHeap<T> {
     pub fn new() -> Self {
-        MeldableHeap {
-            root: Box::new(Nil),
-            len: 0,
-        }
+        MeldableHeap { root: None, len: 0 }
     }
 
-    fn merge(mut h1: Box<Node<T>>, mut h2: Box<Node<T>>) -> Box<Node<T>> {
-        match (h1.as_mut(), h2.as_mut()) {
-            (Nil, _) => {
+    fn merge(mut h1: Option<Box<Node<T>>>, mut h2: Option<Box<Node<T>>>) -> Option<Box<Node<T>>> {
+        match (&mut h1, &mut h2) {
+            (None, _) => {
                 return h2;
             }
-            (_, Nil) => {
+            (_, None) => {
                 return h1;
             }
-            (Cons(ref v1, _, _), Cons(ref v2, _, _)) if v1 > v2 => {
+            (Some(b1), Some(b2)) if b1.value > b2.value => {
                 return Self::merge(h2, h1);
             }
-            (Cons(_, l, r), _) => {
+            (Some(b1), _) => {
                 if rand::random() {
-                    let owned_l = mem::replace(l, Box::new(Nil));
-                    *l = Self::merge(owned_l, h2);
+                    let owned_l = mem::replace(&mut b1.left, None);
+                    b1.left = Self::merge(owned_l, h2);
                 } else {
-                    let owned_r = mem::replace(r, Box::new(Nil));
-                    *r = Self::merge(owned_r, h2);
+                    let owned_r = mem::replace(&mut b1.right, None);
+                    b1.right = Self::merge(owned_r, h2);
                 }
                 return h1;
             }
@@ -49,8 +45,8 @@ impl<T: Ord> MeldableHeap<T> {
     // より Rust らしい実装
     // 本では absorb() となっている
     pub fn append(&mut self, other: &mut MeldableHeap<T>) {
-        let owned_root = mem::replace(&mut self.root, Box::new(Nil));
-        let owned_other_root = mem::replace(&mut other.root, Box::new(Nil));
+        let owned_root = mem::replace(&mut self.root, None);
+        let owned_other_root = mem::replace(&mut other.root, None);
         self.root = Self::merge(owned_root, owned_other_root);
         self.len += other.len;
         other.len = 0;
@@ -58,23 +54,28 @@ impl<T: Ord> MeldableHeap<T> {
 
     // 本では add() となっている
     pub fn insert(&mut self, value: T) {
-        let node = Box::new(Cons(value, Box::new(Nil), Box::new(Nil)));
-        let owned_root = mem::replace(&mut self.root, Box::new(Nil));
+        let node = Some(Box::new(Node {
+            value,
+            left: None,
+            right: None,
+        }));
+        let owned_root = mem::replace(&mut self.root, None);
         self.root = Self::merge(node, owned_root);
         self.len += 1;
     }
 
     // 本では remove() となっている
     pub fn pop(&mut self) -> Option<T> {
-        let owned_root = mem::replace(&mut self.root, Box::new(Nil));
-        match *owned_root {
-            Nil => {
+        let owned_root = mem::replace(&mut self.root, None);
+        match owned_root {
+            None => {
                 return None;
             }
-            Cons(v, l, r) => {
-                self.root = Self::merge(l, r);
+            Some(b) => {
+                let Node { value, left, right } = *b;
+                self.root = Self::merge(left, right);
                 self.len -= 1;
-                return Some(v);
+                return Some(value);
             }
         };
     }
@@ -84,7 +85,7 @@ impl<T: Ord> MeldableHeap<T> {
     }
 
     pub fn is_empty(&self) -> bool {
-        matches!(*self.root, Node::Nil)
+        self.root.is_none()
     }
 }
 
@@ -94,13 +95,13 @@ impl<T: Ord + fmt::Debug> fmt::Debug for MeldableHeap<T> {
         let mut stack = vec![];
         stack.push((0, &self.root));
         while let Some((d, node)) = stack.pop() {
-            match node.as_ref() {
-                Nil => {}
-                Cons(v, l, r) => {
+            match node {
+                None => {}
+                Some(b) => {
                     write!(f, "{}", "    ".repeat(1 + d))?;
-                    writeln!(f, "{:?}", v)?;
-                    stack.push((d + 1, r));
-                    stack.push((d + 1, l));
+                    writeln!(f, "{:?}", b.value)?;
+                    stack.push((d + 1, &b.right));
+                    stack.push((d + 1, &b.left));
                 }
             };
         }
@@ -127,5 +128,6 @@ mod tests {
         h1.append(&mut h2);
         dbg!(&h1);
         assert_eq!(h1.pop(), Some(0));
+        assert!(h2.is_empty());
     }
 }
